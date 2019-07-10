c421999c7cfa08cff674038cf6436f5cd8895e8f
IKoshelev <koshelew@live.com>
2019-07-10 23:26:41 +0200

    Narrowing things inside array elements.

--- src/index.ts
+++ src/index.ts
@@ -9,57 +9,78 @@ export type NarrowPropType<TTarget,
         /**/ : TTarget[key]
     };
 
-export function narrowPropTypeGuard<
-    TTarget,
-    TKey extends keyof TTarget,
-    TWide extends TTarget[TKey],
-    TNarrow extends TWide>(
-        target: TTarget,
-        key: TKey,
-        check: (inst: TWide) => inst is TNarrow
-    ): target is  TTarget & NarrowPropType<TTarget, TKey, TNarrow> {
-
-    const inst = target[key] as TWide;
-    const isExpectedType = check(inst);
-    return isExpectedType;
+export function narrowArrayElement<TWide, TNarrow extends TWide>(check:(i: TWide) => i is TNarrow ){
+    return (i: TWide[]) : i is TNarrow[] => i.every(check); 
 }
 
-export type NarrowPropPropType<TTarget,
-    TKey1 extends keyof TTarget,
-    TKey2 extends keyof TTarget[TKey1],
-    TNarrow extends TTarget[TKey1][TKey2]> = {
-        [outerKey in keyof TTarget]:
-        outerKey extends TKey1
-    /**/ ? TTarget[TKey1] extends TTarget[outerKey]
-    /*  */ ? {
-    /*   |  */[innerKey in keyof TTarget[outerKey]]:
-    /*   |  */ innerKey extends TKey2
-    /*   |   |  */ ? TNarrow extends TTarget[outerKey][innerKey]
-    /*   |   |   |  */ ? TNarrow
-    /*   |   |   |  */ : TTarget[outerKey][innerKey]
-    /*   |   |  */ : TTarget[outerKey][innerKey]
-    /*   |  */
-    /*  */ }
-    /*  */ : TTarget[outerKey]
-    /**/ : TTarget[outerKey]
-    };
+export function narrowArrayElemProp<
+    TOuterArrElem,
+    TKey extends  keyof TOuterArrElem,
+    TNarrowArr extends TOuterArrElem[TKey]>(
+        arr: TOuterArrElem[], 
+        keyInElem: TKey, 
+        check:(i: TOuterArrElem[TKey]) => i is TNarrowArr ): arr is NarrowPropType<TOuterArrElem,TKey,TNarrowArr>[] {
 
-export function narrowPropPropTypeGuard<
-    TTarget,
-    TKey1 extends keyof TTarget,
-    TKey2 extends keyof TTarget[TKey1],
-    TNarrow extends TTarget[TKey1][TKey2]>(
-        target: TTarget,
-        key1: TKey1,
-        key2: TKey2,
-        check: (inst: TTarget[TKey1][TKey2]) => inst is TNarrow
-    ): target is TTarget & NarrowPropPropType<TTarget, TKey1, TKey2, TNarrow> {
-
-        const outer: TTarget[TKey1] = target[key1];
-        if(!outer){
-            return false;
-        }
-        const inner: TTarget[TKey1][TKey2] = outer[key2];
-        const isExpectedType = check(inner);
-        return isExpectedType;
+    return arr.every(e => check(e[keyInElem]));
 }
+
+
+
+
+
+
+
+// export function narrowPropTypeGuard<
+//     TTarget,
+//     TKey extends keyof TTarget,
+//     TWide extends TTarget[TKey],
+//     TNarrow extends TWide>(
+//         target: TTarget,
+//         key: TKey,
+//         check: (inst: TWide) => inst is TNarrow
+//     ): target is  TTarget & NarrowPropType<TTarget, TKey, TNarrow> {
+
+//     const inst = target[key] as TWide;
+//     const isExpectedType = check(inst);
+//     return isExpectedType;
+// }
+
+// export type NarrowPropPropType<TTarget,
+//     TKey1 extends keyof TTarget,
+//     TKey2 extends keyof TTarget[TKey1],
+//     TNarrow extends TTarget[TKey1][TKey2]> = {
+//         [outerKey in keyof TTarget]:
+//         outerKey extends TKey1
+//     /**/ ? TTarget[TKey1] extends TTarget[outerKey]
+//     /*  */ ? {
+//     /*   |  */[innerKey in keyof TTarget[outerKey]]:
+//     /*   |  */ innerKey extends TKey2
+//     /*   |   |  */ ? TNarrow extends TTarget[outerKey][innerKey]
+//     /*   |   |   |  */ ? TNarrow
+//     /*   |   |   |  */ : TTarget[outerKey][innerKey]
+//     /*   |   |  */ : TTarget[outerKey][innerKey]
+//     /*   |  */
+//     /*  */ }
+//     /*  */ : TTarget[outerKey]
+//     /**/ : TTarget[outerKey]
+//     };
+
+// export function narrowPropPropTypeGuard<
+//     TTarget,
+//     TKey1 extends keyof TTarget,
+//     TKey2 extends keyof TTarget[TKey1],
+//     TNarrow extends TTarget[TKey1][TKey2]>(
+//         target: TTarget,
+//         key1: TKey1,
+//         key2: TKey2,
+//         check: (inst: TTarget[TKey1][TKey2]) => inst is TNarrow
+//     ): target is TTarget & NarrowPropPropType<TTarget, TKey1, TKey2, TNarrow> {
+
+//         const outer: TTarget[TKey1] = target[key1];
+//         if(!outer){
+//             return false;
+//         }
+//         const inner: TTarget[TKey1][TKey2] = outer[key2];
+//         const isExpectedType = check(inner);
+//         return isExpectedType;
+// }
--- test/intex.test.ts
+++ test/intex.test.ts
@@ -1,84 +1,112 @@
 import * as mocha from 'mocha';
-import { narrowPropTypeGuard, narrowPropPropTypeGuard } from "../src/index";
+import { narrowArrayElemProp, narrowArrayElement } from "../src/index";
 import { expect } from "chai";
 
 type Subject = {
-    a1: string | number | undefined,
-    a2: {
-        b1: string | number | undefined,
-        control: string
-    },
-    control: string,
+    a1: {
+        b1: (number | string)[],
+        b2: number | string,
+        control: string     
+    }[],
+    control: string
 }
 
 const controlMarker = 'controll-marker';
 
 const getFreshSubject = () => ({
-    a2: {
+    a1: [{
         control: controlMarker
-    }, 
+    }],
     control: controlMarker
 } as Subject)
 
 const isNumber = (i: string | number | undefined): i is number => typeof i === 'number';
 
-describe('narrowPropTypeGuard ', () => {
+describe('narrowArrayElemProp', () => {
 
-    it('when prop passes type guard, returns true and narrows it down', () => {
+    it('can adapt simple type guard to narrow down type of property of array element', () => {
         const subject = getFreshSubject();
 
-        subject.a1 = (8 as  string | number | undefined);
-        if (narrowPropTypeGuard(subject, 'a1', isNumber)) {
-            const narrow: number = subject.a1;          // this line checks that compiler has narrowed type of a1 to number
-            const control: string = subject.control;   // this line checks that compiler has not narrowed type of  to number
+        subject.a1[0].b2 = (8 as  string | number);
+        if (narrowArrayElemProp(subject.a1, 'b2', isNumber)) {
+
+             // this line checks that compiler has narrowed type of a1 to number
+            const narrow: number = subject.a1[0].b2;
             expect(narrow).to.equal(8);
-            expect(control).to.equal(controlMarker);
+
+            // this lines check that compiler has not narrowed type of  to number
+            const control1: string = subject.control;   
+            expect(control1).to.equal(controlMarker);
+
+            const control2: string = subject.a1[0].control;   
+            expect(control2).to.equal(controlMarker);
         } else {
             throw new Error('Should not get here.');
         }
     });
 
-    it('when prop does not pass type guard, returns false', () => {
+    it('when not every element passes the test, returns false', () => {
+
         const subject = getFreshSubject();
-        subject.a1 = ('str' as  string | number | undefined);
-        if (narrowPropTypeGuard(subject, 'a1', isNumber)) {
+        subject.a1[0].b2 = ('a' as  string | number);
+
+        if (narrowArrayElemProp(subject.a1, 'b2', isNumber)) {
             throw new Error('Should not get here.');
         } else {
             const control: string = subject.control;   // this line checks that compiler has not narrowed type of control to number
-            expect(subject.a1).to.equal('str');
             expect(control).to.equal(controlMarker);
         }
     });
 });
 
-describe('narrowPropPropTypeGuard ', () => {
+describe('narrowArrayElemProp + narrowArrayElement', () => {
 
-    it('when prop passes type guard, returns true and narrows it down', () => {
+    it('can narrow down array element property that is itself array', () => {
         const subject = getFreshSubject();
-        subject.a2.b1 = (8 as  string | number | undefined);
-        if (narrowPropPropTypeGuard(subject, 'a2','b1', isNumber)) {
-            const narrow: number = subject.a2.b1;       // this line checks that compiler has narrowed type of a1 to number
-            const control1: string = subject.a2.control;   // this line checks that compiler has not narrowed type of control to number
-            const control2: string = subject.control;
+
+        subject.a1[0].b1 = [(8 as  string | number)];
+        if (narrowArrayElemProp(subject.a1, 'b1', narrowArrayElement(isNumber))) {
+
+             // this line checks that compiler has narrowed type of a1 to number
+            const narrow: number = subject.a1[0].b1[0];
             expect(narrow).to.equal(8);
+
+            // this lines check that compiler has not narrowed type of  to number
+            const control1: string = subject.control;   
             expect(control1).to.equal(controlMarker);
+
+            const control2: string = subject.a1[0].control;   
             expect(control2).to.equal(controlMarker);
         } else {
             throw new Error('Should not get here.');
         }
     });
 
-    it('when prop does not pass type guard, returns false', () => {
+    it('when not every inner element of every outer elemnt passes the test, returns false', () => {
+
         const subject = getFreshSubject();
-        subject.a2.b1 = ('str' as  string | number | undefined);
-        if (narrowPropPropTypeGuard(subject, 'a2','b1', isNumber)) {
+        subject.a1[0].b1 = [(8 as  string | number)];
+
+        if (narrowArrayElemProp(subject.a1, 'b1', narrowArrayElement(isNumber))) {
+            expect(true).to.equal(true);
+       } else {
+           // can't just throw here, because clever compiler 
+           // deduces that execution can only continue on successfull type check
+           // and extends narrowing to the rest of the method
+            expect(false).to.equal(true);
+       }
+
+        subject.a1[1] = {
+            b1: [('a' as  string | number)],
+            b2: '',
+            control: controlMarker
+        };
+
+        if (narrowArrayElemProp(subject.a1, 'b1', narrowArrayElement(isNumber))) {
             throw new Error('Should not get here.');
         } else {
-            const control1: string = subject.a2.control;   // this line checks that compiler has not narrowed type of control to number
-            const control2: string = subject.control;
-            expect(subject.a2.b1).to.equal('str');
-            expect(control1).to.equal(controlMarker);
-            expect(control2).to.equal(controlMarker);
+            const control: string = subject.control;   // this line checks that compiler has not narrowed type of control to number
+            expect(control).to.equal(controlMarker);
         }
     });
 });
